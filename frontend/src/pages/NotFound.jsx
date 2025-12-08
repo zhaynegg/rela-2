fd920f86ae7bbbf8e9503e1214343a3915bc5bb9
Zhassyn Yegnay <zhassynyegnay@Zhassyns-MacBook-Air.local>
2025-12-08 17:52:37 +0500

    implemented form

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,4 +1,7 @@
 function NotFound() {
-    return <div> Not found </div>
+    return <div>
+        <h1>404 Not found</h1>
+        <p>page you're looking for doesn't exist</p>
+    </div>
 }
 export default NotFound
